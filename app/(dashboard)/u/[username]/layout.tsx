1c3cbeb661ac07dbb4df202ff7350ca394dec9d8
anonymousFPP <abhaygaming1412001@gmail.com>
2024-09-05 19:52:19 +0530

    changes quotes

--- app/(dashboard)/u/[username]/layout.tsx
+++ app/(dashboard)/u/[username]/layout.tsx
@@ -1,5 +1,5 @@
 import { getSelfByUsername } from "@/lib/auth-service";
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
 import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
 import { Container } from "./_components/container";
